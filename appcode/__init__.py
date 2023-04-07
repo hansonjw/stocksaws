4f484ad8d30ed0bf08af29f85ce3796c2ba0c106
hansonjw <hansonjw@gmail.com>
2023-04-07 16:49:50 -0500

    Updated name from app to application for aws hosting

--- appcode/__init__.py
+++ appcode/__init__.py
@@ -3,33 +3,33 @@ from flask import Flask
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
+    application = Flask(__name__, instance_relative_config=True)
+    application.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(application.instance_path, 'flaskr.sqlite'),
     )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        application.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed in
-        app.config.from_mapping(test_config)
+        application.config.from_mapping(test_config)
 
     # ensure the instance folder exists
     try:
-        os.makedirs(app.instance_path)
+        os.makedirs(application.instance_path)
     except OSError:
         pass
 
     # a simple page that says hello
-    @app.route('/')
+    @application.route('/')
     def hello():
         return '<p>Hello, AWS!</p><a href="/today">today link</a>'
 
     import appcode.routes.routes as routes
-    app.register_blueprint(routes.bp)
+    application.register_blueprint(routes.bp)
    
-    return app
+    return application
 
 application = create_app()
